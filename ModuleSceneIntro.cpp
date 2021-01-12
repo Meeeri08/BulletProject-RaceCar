ce10ce0ba335544f9784cca8a1f3810d9f46e1da
MaxLlovera <61042419+MaxLlovera@users.noreply.github.com>
2021-01-13 00:54:25 +0100

    start, color changes and walls fixed
    start, color changes and walls fixed

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -27,21 +27,33 @@ bool ModuleSceneIntro::Start()
 	//App->camera->LookAt(vec3(0, 0, 0));
 
 	//Collisions
+	Cube wallGround(290, 1, 600);
+	wallGround.SetPos(0, 0, 20);
+	App->physics->AddBody(wallGround, 0);
+
+	///START
+	Cube start1(1, 10, 1);
+	start1.SetPos(15, 10, 10);
+	App->physics->AddBody(start1, 0);
+
+
+	Cube start2(1, 10, 1);
+	start2.SetPos(-15, 10, 10);
+	App->physics->AddBody(start2, 0);
+
 	//limits
 	Cube wall1(310, 50, 10);
-	wall1.color.Set(1.0f, 0.0f, 0.0f);
 	wall1.SetPos(0, 0, -100);
 	App->physics->AddBody(wall1, 0);
 
-	Cube wall2(10, 50, 300);
-	wall2.color.Set(1.0f, 0.0f, 0.0f);
+	Cube wall2(10, 50, 500);
 	wall2.SetPos(-150, 0, 155);
 	App->physics->AddBody(wall2, 0);
 
-	Cube wall3(10, 50, 300);
-	wall3.color.Set(1.0f, 0.0f, 0.0f);
+	Cube wall3(10, 50, 500);
 	wall3.SetPos(150, 0, 155);
 	App->physics->AddBody(wall3, 0);
+
 
 	//Cube wall4(310, 50, 10);
 	//wall4.SetPos(0, 0, 310);
@@ -50,130 +62,108 @@ bool ModuleSceneIntro::Start()
 	//circuit
 
 	Cube wall5(2, 10, 100);
-	wall5.color.Set(1.0f, 0.0f, 0.0f);
 	wall5.SetPos(-15, 0, 30);
 	App->physics->AddBody(wall5, 0);
 
 	Cube wall6(2, 10, 130);
-	wall6.color.Set(1.0f, 0.0f, 0.0f);
 	wall6.SetPos(15, 0, 45);
 	App->physics->AddBody(wall6, 0);
 
 	Cube wall7(32, 10, 2);
-	wall7.color.Set(1.0f, 0.0f, 0.0f);
 	wall7.SetPos(0, 0, -20);
 	App->physics->AddBody(wall7, 0);
 
+
 	Cube wall8(70, 10, 2);
-	wall8.color.Set(1.0f, 0.0f, 0.0f);
-	wall8.SetPos(-60, 0, 80);
+	wall8.SetPos(-50, 0, 80);
 	App->physics->AddBody(wall8, 0);
 
 	Cube wall9(70, 10, 2);
-	wall9.color.Set(1.0f, 0.0f, 0.0f);
 	wall9.SetPos(-20, 0, 110);
 	App->physics->AddBody(wall9, 0);
 
-	Cube wall10(2, 10, 70);
-	wall10.color.Set(1.0f, 0.0f, 0.0f);
-	wall10.SetPos(-85, 0, 110);
+	Cube wall10(2, 10, 65);
+	wall10.SetPos(-85, 0, 113);
 	App->physics->AddBody(wall10, 0);
 
 	Cube wall11(150, 10, 2);
-	wall11.color.Set(1.0f, 0.0f, 0.0f);
 	wall11.SetPos(-10, 0, 145);
 	App->physics->AddBody(wall11, 0);
 
 	Cube wall12(2, 10, 110);
-	wall12.color.Set(1.0f, 0.0f, 0.0f);
 	wall12.SetPos(65, 0, 95);
 	App->physics->AddBody(wall12, 0);
 
 	Cube wall13(50, 10, 2);
-	wall13.color.Set(1.0f, 0.0f, 0.0f);
 	wall13.SetPos(90, 0, 40);
 	App->physics->AddBody(wall13, 0);
 
 	Cube wall14(60, 10, 2);
-	wall14.color.Set(1.0f, 0.0f, 0.0f);
 	wall14.SetPos(45, 0, 10);
 	App->physics->AddBody(wall14, 0);
 
 	Cube wall15(2, 10, 80);
-	wall15.color.Set(1.0f, 0.0f, 0.0f);
 	wall15.SetPos(115, 0, 0);
 	App->physics->AddBody(wall15, 0);
 
 	Cube wall16(2, 10, 70);
-	wall16.color.Set(1.0f, 0.0f, 0.0f);
 	wall16.SetPos(75, 0, -25);
 	App->physics->AddBody(wall16, 0);
 
 	Cube wall17(70, 10, 2);
-	wall17.color.Set(1.0f, 0.0f, 0.0f);
 	wall17.SetPos(110, 0, -60);
 	App->physics->AddBody(wall17, 0);
 
 	Cube wall18(50, 10, 2);
-	wall8.color.Set(1.0f, 0.0f, 0.0f);
 	wall18.SetPos(120, 0, 70);
 	App->physics->AddBody(wall18, 0);
 
 	Cube wall19(2, 10, 40);
-	wall19.color.Set(1.0f, 0.0f, 0.0f);
 	wall19.SetPos(95, 0, 90);
 	App->physics->AddBody(wall19, 0);
 
+	////zone 2
 
 	Cube ramp1(30, 60, 1);
-	ramp1.color.Set(1.0f, 0.0f, 0.0f);
 	ramp1.SetPos(80, 5, 100);
 	ramp1.SetRotation(90 - 20, { 1,0,0 });
 	App->physics->AddBody(ramp1, 0);
 
-
-	Cube wall20(2, 30, 150);
-	wall20.color.Set(1.0f, 0.0f, 0.0f);
-	wall20.SetPos(95, 10, 170);
+	//hight walls
+	Cube wall20(2, 30, 180);
+	wall20.SetPos(95, 10, 185);
 	App->physics->AddBody(wall20, 0);
 
 	Cube wall21(2, 30, 150);
-	wall21.color.Set(1.0f, 0.0f, 0.0f);
 	wall21.SetPos(65, 10, 170);
 	App->physics->AddBody(wall21, 0);
 
+	//floating ground
 	Cube wall22(30, 2, 150);
-	wall22.color.Set(1.0f, 0.0f, 0.0f);
 	wall22.SetPos(80, 10, 195);
 	App->physics->AddBody(wall22, 0);
 
 	Cube wall23(120, 2, 30);
-	wall23.color.Set(1.0f, 0.0f, 0.0f);
 	wall23.SetPos(40, 10, 260);
 	App->physics->AddBody(wall23, 0);
 
 	Cube wall24(30, 2, 100);
-	wall24.color.Set(1.0f, 0.0f, 0.0f);
 	wall24.SetPos(-35, 10, 295);
 	App->physics->AddBody(wall24, 0);
 
 	Cube wall25(10, 2, 100);
-	wall25.color.Set(1.0f, 0.0f, 0.0f);
 	wall25.SetPos(-35, 10, 395);
 	App->physics->AddBody(wall25, 0);
 
 	Cube wall26(80, 2, 10);
-	wall26.color.Set(1.0f, 0.0f, 0.0f);
 	wall26.SetPos(0, 10, 440);
 	App->physics->AddBody(wall26, 0);
 
 	Cube wall27(10, 2, 100);
-	wall27.color.Set(1.0f, 0.0f, 0.0f);
 	wall27.SetPos(40, 10, 485);
 	App->physics->AddBody(wall27, 0);
 
 	Cube wall28(10, 2, 110);
-	wall28.color.Set(1.0f, 0.0f, 0.0f);
 	wall28.SetPos(40, 10, 620);
 	App->physics->AddBody(wall28, 0);
 	
@@ -208,19 +198,474 @@ update_status ModuleSceneIntro::Update(float dt)
 {
 	ground->Render();
 
+
+	//ground
+	Cube wallGround(290, 1, 600);
+	wallGround.color.Set(0.5f, 0.5f, 0.67f);
+	wallGround.SetPos(0, 0, 20);
+	wallGround.Render();
+
+	//START
+	Cube start1(1, 10, 1);
+	start1.SetPos(15, 10, 10);
+	start1.Render();
+
+
+	Cube start2(1, 10, 1);
+	start2.SetPos(-15, 10, 10);
+	start2.Render();
+
+	////////////////////////////////////////////////////////
+	//Flag
+	Cube flag1(1, 1, 1);
+	flag1.color.Set(0.0f, 0.0f, 0.0f);
+	flag1.SetPos(-14, 14, 10);
+	flag1.Render();
+
+	Cube flag2(1, 1, 1);
+	flag2.color.Set(1.0f, 1.0f, 1.0f);
+	flag2.SetPos(-14, 13, 10);
+	flag2.Render();
+
+	Cube flag3(1, 1, 1);
+	flag3.color.Set(0.0f, 0.0f, 0.0f);
+	flag3.SetPos(-14, 12, 10);
+	flag3.Render();
+
+	Cube flag4(1, 1, 1);
+	flag4.color.Set(1.0f, 1.0f, 1.0f);
+	flag4.SetPos(-13, 14, 10);
+	flag4.Render();
+
+	Cube flag5(1, 1, 1);
+	flag5.color.Set(0.0f, 0.0f, 0.0f);
+	flag5.SetPos(-13, 13, 10);
+	flag5.Render();
+
+	Cube flag6(1, 1, 1);
+	flag6.color.Set(1.0f, 1.0f, 1.0f);
+	flag6.SetPos(-13, 12, 10);
+	flag6.Render();
+
+	Cube flag7(1, 1, 1);
+	flag7.color.Set(0.0f, 0.0f, 0.0f);
+	flag7.SetPos(-12, 14, 10);
+	flag7.Render();
+
+	Cube flag8(1, 1, 1);
+	flag8.color.Set(1.0f, 1.0f, 1.0f);
+	flag8.SetPos(-12, 13, 10);
+	flag8.Render();
+
+	Cube flag9(1, 1, 1);
+	flag9.color.Set(0.0f, 0.0f, 0.0f);
+	flag9.SetPos(-12, 12, 10);
+	flag9.Render();
+
+	Cube flag10(1, 1, 1);
+	flag10.color.Set(1.0f, 1.0f, 1.0f);
+	flag10.SetPos(-11, 14, 10);
+	flag10.Render();
+
+	Cube flag11(1, 1, 1);
+	flag11.color.Set(0.0f, 0.0f, 0.0f);
+	flag11.SetPos(-11, 13, 10);
+	flag11.Render();
+
+	Cube flag12(1, 1, 1);
+	flag12.color.Set(1.0f, 1.0f, 1.0f);
+	flag12.SetPos(-11, 12, 10);
+	flag12.Render();
+
+	Cube flag13(1, 1, 1);
+	flag13.color.Set(0.0f, 0.0f, 0.0f);
+	flag13.SetPos(-10, 14, 10);
+	flag13.Render();
+
+	Cube flag14(1, 1, 1);
+	flag14.color.Set(1.0f, 1.0f, 1.0f);
+	flag14.SetPos(-10, 13, 10);
+	flag14.Render();
+
+	Cube flag15(1, 1, 1);
+	flag15.color.Set(0.0f, 0.0f, 0.0f);
+	flag15.SetPos(-10, 12, 10);
+	flag15.Render();
+
+	Cube flag16(1, 1, 1);
+	flag16.color.Set(1.0f, 1.0f, 1.0f);
+	flag16.SetPos(-9, 14, 10);
+	flag16.Render();
+
+	Cube flag17(1, 1, 1);
+	flag17.color.Set(0.0f, 0.0f, 0.0f);
+	flag17.SetPos(-9, 13, 10);
+	flag17.Render();
+
+	Cube flag18(1, 1, 1);
+	flag18.color.Set(1.0f, 1.0f, 1.0f);
+	flag18.SetPos(-9, 12, 10);
+	flag18.Render();
+
+	Cube flag19(1, 1, 1);
+	flag19.color.Set(0.0f, 0.0f, 0.0f);
+	flag19.SetPos(-8, 14, 10);
+	flag19.Render();
+
+	Cube flag20(1, 1, 1);
+	flag20.color.Set(1.0f, 1.0f, 1.0f);
+	flag20.SetPos(-8, 13, 10);
+	flag20.Render();
+
+	Cube flag21(1, 1, 1);
+	flag21.color.Set(0.0f, 0.0f, 0.0f);
+	flag21.SetPos(-8, 12, 10);
+	flag21.Render();
+
+	Cube flag22(1, 1, 1);
+	flag22.color.Set(1.0f, 1.0f, 1.0f);
+	flag22.SetPos(-7, 14, 10);
+	flag22.Render();
+
+	Cube flag23(1, 1, 1);
+	flag23.color.Set(0.0f, 0.0f, 0.0f);
+	flag23.SetPos(-7, 13, 10);
+	flag23.Render();
+
+	Cube flag24(1, 1, 1);
+	flag24.color.Set(1.0f, 1.0f, 1.0f);
+	flag24.SetPos(-7, 12, 10);
+	flag24.Render();
+
+	Cube flag25(1, 1, 1);
+	flag25.color.Set(0.0f, 0.0f, 0.0f);
+	flag25.SetPos(-6, 14, 10);
+	flag25.Render();
+
+	Cube flag26(1, 1, 1);
+	flag26.color.Set(1.0f, 1.0f, 1.0f);
+	flag26.SetPos(-6, 13, 10);
+	flag26.Render();
+
+	Cube flag27(1, 1, 1);
+	flag27.color.Set(0.0f, 0.0f, 0.0f);
+	flag27.SetPos(-6, 12, 10);
+	flag27.Render();
+
+	Cube flag28(1, 1, 1);
+	flag28.color.Set(1.0f, 1.0f, 1.0f);
+	flag28.SetPos(-5, 14, 10);
+	flag28.Render();
+
+	Cube flag29(1, 1, 1);
+	flag29.color.Set(0.0f, 0.0f, 0.0f);
+	flag29.SetPos(-5, 13, 10);
+	flag29.Render();
+
+	Cube flag30(1, 1, 1);
+	flag30.color.Set(1.0f, 1.0f, 1.0f);
+	flag30.SetPos(-5, 12, 10);
+	flag30.Render();
+
+	Cube flag31(1, 1, 1);
+	flag31.color.Set(0.0f, 0.0f, 0.0f);
+	flag31.SetPos(-4, 14, 10);
+	flag31.Render();
+
+	Cube flag32(1, 1, 1);
+	flag32.color.Set(1.0f, 1.0f, 1.0f);
+	flag32.SetPos(-4, 13, 10);
+	flag32.Render();
+
+	Cube flag33(1, 1, 1);
+	flag33.color.Set(0.0f, 0.0f, 0.0f);
+	flag33.SetPos(-4, 12, 10);
+	flag33.Render();
+
+	Cube flag34(1, 1, 1);
+	flag34.color.Set(1.0f, 1.0f, 1.0f);
+	flag34.SetPos(-3, 14, 10);
+	flag34.Render();
+
+	Cube flag35(1, 1, 1);
+	flag35.color.Set(0.0f, 0.0f, 0.0f);
+	flag35.SetPos(-3, 13, 10);
+	flag35.Render();
+
+	Cube flag36(1, 1, 1);
+	flag36.color.Set(1.0f, 1.0f, 1.0f);
+	flag36.SetPos(-3, 12, 10);
+	flag36.Render();
+
+	Cube flag37(1, 1, 1);
+	flag37.color.Set(0.0f, 0.0f, 0.0f);
+	flag37.SetPos(-2, 14, 10);
+	flag37.Render();
+
+	Cube flag38(1, 1, 1);
+	flag38.color.Set(1.0f, 1.0f, 1.0f);
+	flag38.SetPos(-2, 13, 10);
+	flag38.Render();
+
+	Cube flag39(1, 1, 1);
+	flag39.color.Set(0.0f, 0.0f, 0.0f);
+	flag39.SetPos(-2, 12, 10);
+	flag39.Render();
+
+	Cube flag40(1, 1, 1);
+	flag40.color.Set(1.0f, 1.0f, 1.0f);
+	flag40.SetPos(-1, 14, 10);
+	flag40.Render();
+
+	Cube flag41(1, 1, 1);
+	flag41.color.Set(0.0f, 0.0f, 0.0f);
+	flag41.SetPos(-1, 13, 10);
+	flag41.Render();
+
+	Cube flag42(1, 1, 1);
+	flag42.color.Set(1.0f, 1.0f, 1.0f);
+	flag42.SetPos(-1, 12, 10);
+	flag42.Render();
+
+	Cube flag43(1, 1, 1);
+	flag43.color.Set(0.0f, 0.0f, 0.0f);
+	flag43.SetPos(0, 14, 10);
+	flag43.Render();
+
+	Cube flag44(1, 1, 1);
+	flag44.color.Set(1.0f, 1.0f, 1.0f);
+	flag44.SetPos(0, 13, 10);
+	flag44.Render();
+
+	Cube flag45(1, 1, 1);
+	flag45.color.Set(0.0f, 0.0f, 0.0f);
+	flag45.SetPos(0, 12, 10);
+	flag45.Render();
+
+	Cube flag46(1, 1, 1);
+	flag46.color.Set(1.0f, 1.0f, 1.0f);
+	flag46.SetPos(1, 14, 10);
+	flag46.Render();
+
+	Cube flag47(1, 1, 1);
+	flag47.color.Set(0.0f, 0.0f, 0.0f);
+	flag47.SetPos(1, 13, 10);
+	flag47.Render();
+
+	Cube flag48(1, 1, 1);
+	flag48.color.Set(1.0f, 1.0f, 1.0f);
+	flag48.SetPos(1, 12, 10);
+	flag48.Render();
+
+	Cube flag49(1, 1, 1);
+	flag49.color.Set(0.0f, 0.0f, 0.0f);
+	flag49.SetPos(2, 14, 10);
+	flag49.Render();
+
+	Cube flag50(1, 1, 1);
+	flag50.color.Set(1.0f, 1.0f, 1.0f);
+	flag50.SetPos(2, 13, 10);
+	flag50.Render();
+
+	Cube flag51(1, 1, 1);
+	flag51.color.Set(0.0f, 0.0f, 0.0f);
+	flag51.SetPos(2, 12, 10);
+	flag51.Render();
+
+	Cube flag52(1, 1, 1);
+	flag52.color.Set(1.0f, 1.0f, 1.0f);
+	flag52.SetPos(3, 14, 10);
+	flag52.Render();
+
+	Cube flag53(1, 1, 1);
+	flag53.color.Set(0.0f, 0.0f, 0.0f);
+	flag53.SetPos(3, 13, 10);
+	flag53.Render();
+
+	Cube flag54(1, 1, 1);
+	flag54.color.Set(1.0f, 1.0f, 1.0f);
+	flag54.SetPos(3, 12, 10);
+	flag54.Render();
+
+	Cube flag55(1, 1, 1);
+	flag55.color.Set(0.0f, 0.0f, 0.0f);
+	flag55.SetPos(4, 14, 10);
+	flag55.Render();
+
+	Cube flag56(1, 1, 1);
+	flag56.color.Set(1.0f, 1.0f, 1.0f);
+	flag56.SetPos(4, 13, 10);
+	flag56.Render();
+
+	Cube flag57(1, 1, 1);
+	flag57.color.Set(0.0f, 0.0f, 0.0f);
+	flag57.SetPos(4, 12, 10);
+	flag57.Render();
+
+	Cube flag58(1, 1, 1);
+	flag58.color.Set(1.0f, 1.0f, 1.0f);
+	flag58.SetPos(5, 14, 10);
+	flag58.Render();
+
+	Cube flag59(1, 1, 1);
+	flag59.color.Set(0.0f, 0.0f, 0.0f);
+	flag59.SetPos(5, 13, 10);
+	flag59.Render();
+
+	Cube flag60(1, 1, 1);
+	flag60.color.Set(1.0f, 1.0f, 1.0f);
+	flag60.SetPos(5, 12, 10);
+	flag60.Render();
+
+	Cube flag61(1, 1, 1);
+	flag61.color.Set(0.0f, 0.0f, 0.0f);
+	flag61.SetPos(6, 14, 10);
+	flag61.Render();
+
+	Cube flag62(1, 1, 1);
+	flag62.color.Set(1.0f, 1.0f, 1.0f);
+	flag62.SetPos(6, 13, 10);
+	flag62.Render();
+
+	Cube flag63(1, 1, 1);
+	flag63.color.Set(0.0f, 0.0f, 0.0f);
+	flag63.SetPos(6, 12, 10);
+	flag63.Render();
+
+	Cube flag64(1, 1, 1);
+	flag64.color.Set(1.0f, 1.0f, 1.0f);
+	flag64.SetPos(7, 14, 10);
+	flag64.Render();
+
+	Cube flag65(1, 1, 1);
+	flag65.color.Set(0.0f, 0.0f, 0.0f);
+	flag65.SetPos(7, 13, 10);
+	flag65.Render();
+
+	Cube flag66(1, 1, 1);
+	flag66.color.Set(1.0f, 1.0f, 1.0f);
+	flag66.SetPos(7, 12, 10);
+	flag66.Render();
+
+	Cube flag67(1, 1, 1);
+	flag67.color.Set(0.0f, 0.0f, 0.0f);
+	flag67.SetPos(8, 14, 10);
+	flag67.Render();
+
+	Cube flag68(1, 1, 1);
+	flag68.color.Set(1.0f, 1.0f, 1.0f);
+	flag68.SetPos(8, 13, 10);
+	flag68.Render();
+
+	Cube flag69(1, 1, 1);
+	flag69.color.Set(0.0f, 0.0f, 0.0f);
+	flag69.SetPos(8, 12, 10);
+	flag69.Render();
+
+	Cube flag70(1, 1, 1);
+	flag70.color.Set(1.0f, 1.0f, 1.0f);
+	flag70.SetPos(9, 14, 10);
+	flag70.Render();
+
+	Cube flag71(1, 1, 1);
+	flag71.color.Set(0.0f, 0.0f, 0.0f);
+	flag71.SetPos(9, 13, 10);
+	flag71.Render();
+
+	Cube flag72(1, 1, 1);
+	flag72.color.Set(1.0f, 1.0f, 1.0f);
+	flag72.SetPos(9, 12, 10);
+	flag72.Render();
+
+	Cube flag73(1, 1, 1);
+	flag73.color.Set(0.0f, 0.0f, 0.0f);
+	flag73.SetPos(10, 14, 10);
+	flag73.Render();
+
+	Cube flag74(1, 1, 1);
+	flag74.color.Set(1.0f, 1.0f, 1.0f);
+	flag74.SetPos(10, 13, 10);
+	flag74.Render();
+
+	Cube flag75(1, 1, 1);
+	flag75.color.Set(0.0f, 0.0f, 0.0f);
+	flag75.SetPos(10, 12, 10);
+	flag75.Render();
+
+	Cube flag76(1, 1, 1);
+	flag76.color.Set(1.0f, 1.0f, 1.0f);
+	flag76.SetPos(11, 14, 10);
+	flag76.Render();
+
+	Cube flag77(1, 1, 1);
+	flag77.color.Set(0.0f, 0.0f, 0.0f);
+	flag77.SetPos(11, 13, 10);
+	flag77.Render();
+
+	Cube flag78(1, 1, 1);
+	flag78.color.Set(1.0f, 1.0f, 1.0f);
+	flag78.SetPos(11, 12, 10);
+	flag78.Render();
+
+	Cube flag79(1, 1, 1);
+	flag78.color.Set(0.0f, 0.0f, 0.0f);
+	flag78.SetPos(12, 14, 10);
+	flag78.Render();
+
+	Cube flag80(1, 1, 1);
+	flag80.color.Set(1.0f, 1.0f, 1.0f);
+	flag80.SetPos(12, 13, 10);
+	flag80.Render();
+
+	Cube flag81(1, 1, 1);
+	flag81.color.Set(0.0f, 0.0f, 0.0f);
+	flag81.SetPos(12, 12, 10);
+	flag81.Render();
+
+	Cube flag82(1, 1, 1);
+	flag82.color.Set(1.0f, 1.0f, 1.0f);
+	flag82.SetPos(13, 14, 10);
+	flag82.Render();
+
+	Cube flag83(1, 1, 1);
+	flag83.color.Set(0.0f, 0.0f, 0.0f);
+	flag83.SetPos(13, 13, 10);
+	flag83.Render();
+
+	Cube flag84(1, 1, 1);
+	flag84.color.Set(1.0f, 1.0f, 1.0f);
+	flag84.SetPos(13, 12, 10);
+	flag84.Render();
+
+	Cube flag85(1, 1, 1);
+	flag84.color.Set(0.0f, 0.0f, 0.0f);
+	flag84.SetPos(14, 14, 10);
+	flag84.Render();
+
+	Cube flag86(1, 1, 1);
+	flag86.color.Set(1.0f, 1.0f, 1.0f);
+	flag86.SetPos(14, 13, 10);
+	flag86.Render();
+
+	Cube flag87(1, 1, 1);
+	flag87.color.Set(0.0f, 0.0f, 0.0f);
+	flag87.SetPos(14, 12, 10);
+	flag87.Render();
+
+	///////////////////////////////////////////////////////////////
 	//limits
 	Cube wall1(310, 50, 10);
-	wall1.color.Set(1.0f, 0.0f, 0.0f);
+	wall1.color.Set(1.0f, 1.0f, 0.4f);
 	wall1.SetPos(0, 0, -100);
 	wall1.Render();
 
 	Cube wall2(10, 50, 500);
-	wall2.color.Set(1.0f, 0.0f, 0.0f);
+	wall2.color.Set(1.0f, 1.0f, 0.4f);
 	wall2.SetPos(-150, 0, 155);
 	wall2.Render();
 
 	Cube wall3(10, 50, 500);
-	wall3.color.Set(1.0f, 0.0f, 0.0f);
+	wall3.color.Set(1.0f, 1.0f, 0.4f);
 	wall3.SetPos(150, 0, 155);
 	wall3.Render();
 
@@ -230,128 +675,133 @@ update_status ModuleSceneIntro::Update(float dt)
 
 	//circuit
 	Cube wall5(2, 10, 100);
-	wall5.color.Set(1.0f, 0.0f, 0.0f);
+	wall5.color.Set(1.0f, 0.5f, 0.25f);
 	wall5.SetPos(-15, 0, 30);
 	wall5.Render();
 
 	Cube wall6(2, 10, 130);
-	wall6.color.Set(1.0f, 0.0f, 0.0f);
+	wall6.color.Set(1.0f, 0.5f, 0.25f);
 	wall6.SetPos(15, 0, 45);
 	wall6.Render();
 
 	Cube wall7(32, 10, 2);
-	wall7.color.Set(1.0f, 0.0f, 0.0f);
+	wall7.color.Set(1.0f, 0.5f, 0.25f);
 	wall7.SetPos(0, 0, -20);
 	wall7.Render();
 
 	Cube wall8(70, 10, 2);
-	wall8.color.Set(1.0f, 0.0f, 0.0f);
+	wall8.color.Set(1.0f, 0.5f, 0.25f);
 	wall8.SetPos(-50, 0, 80);
 	wall8.Render();
 
 	Cube wall9(70, 10, 2);
-	wall9.color.Set(1.0f, 0.0f, 0.0f);
+	wall9.color.Set(1.0f, 0.5f, 0.25f);
 	wall9.SetPos(-20, 0, 110);
 	wall9.Render();
 
-	Cube wall10(2, 10, 70);
-	wall10.color.Set(1.0f, 0.0f, 0.0f);
-	wall10.SetPos(-85, 0, 110);
+	Cube wall10(2, 10, 65);
+	wall10.color.Set(1.0f, 0.5f, 0.25f);
+	wall10.SetPos(-85, 0, 113);
 	wall10.Render();
 
 	Cube wall11(150, 10, 2);
-	wall11.color.Set(1.0f, 0.0f, 0.0f);
+	wall11.color.Set(1.0f, 0.5f, 0.25f);
 	wall11.SetPos(-10, 0, 145);
 	wall11.Render();
 
 	Cube wall12(2, 10, 110);
-	wall12.color.Set(1.0f, 0.0f, 0.0f);
+	wall12.color.Set(1.0f, 0.5f, 0.25f);
 	wall12.SetPos(65, 0, 95);
 	wall12.Render();
 
 	Cube wall13(50, 10, 2);
-	wall13.color.Set(1.0f, 0.0f, 0.0f);
+	wall13.color.Set(1.0f, 0.5f, 0.25f);
 	wall13.SetPos(90, 0, 40);
 	wall13.Render();
 
 	Cube wall14(60, 10, 2);
-	wall14.color.Set(1.0f, 0.0f, 0.0f);
+	wall14.color.Set(1.0f, 0.5f, 0.25f);
 	wall14.SetPos(45, 0, 10);
 	wall14.Render();
 
 	Cube wall15(2, 10, 80);
-	wall15.color.Set(1.0f, 0.0f, 0.0f);
+	wall15.color.Set(1.0f, 0.5f, 0.25f);
 	wall15.SetPos(115, 0, 0);
 	wall15.Render();
 
 	Cube wall16(2, 10, 70);
-	wall16.color.Set(1.0f, 0.0f, 0.0f);
+	wall16.color.Set(1.0f, 0.5f, 0.25f);
 	wall16.SetPos(75, 0, -25);
 	wall16.Render();
 
 	Cube wall17(70, 10, 2);
-	wall17.color.Set(1.0f, 0.0f, 0.0f);
+	wall17.color.Set(1.0f, 0.5f, 0.25f);
 	wall17.SetPos(110, 0, -60);
 	wall17.Render();
 
 	Cube wall18(50, 10, 2);
-	wall18.color.Set(1.0f, 0.0f, 0.0f);
+	wall18.color.Set(1.0f, 0.5f, 0.25f);
 	wall18.SetPos(120, 0, 70);
 	wall18.Render();
 
 	Cube wall19(2, 10, 40);
-	wall19.color.Set(1.0f, 0.0f, 0.0f);
+	wall19.color.Set(1.0f, 0.5f, 0.25f);
 	wall19.SetPos(95, 0, 90);
 	wall19.Render();
 
+	/////zone 2
 	Cube ramp1(30, 60, 1);
-	ramp1.color.Set(1.0f, 0.0f, 0.0f);
+	ramp1.color.Set(0.5f, 0.5f, 0.67f);
 	ramp1.SetRotation(90 - 20, { 1,0,0 });
 	ramp1.SetPos(80, 5, 100);
 	ramp1.Render();
 
-	Cube wall20(2, 30, 150);
-	wall20.color.Set(1.0f, 0.0f, 0.0f);
-	wall20.SetPos(95, 10, 170);
+	//high walls
+	Cube wall20(2, 30, 180);
+	wall20.color.Set(1.0f, 0.5f, 0.25f);
+	wall20.SetPos(95, 10, 185);
 	wall20.Render();
 
+
 	Cube wall21(2, 30, 150);
-	wall21.color.Set(1.0f, 0.0f, 0.0f);
+	wall21.color.Set(1.0f, 0.5f, 0.25f);
 	wall21.SetPos(65, 10, 170);
 	wall21.Render();
 
+	//floating ground
 	Cube wall22(30, 2, 150);
-	wall22.color.Set(1.0f, 0.0f, 0.0f);
+	wall22.color.Set(0.6f, 0.2f, 1.0f);
 	wall22.SetPos(80, 10, 195);
 	wall22.Render();
 
+
 	Cube wall23(120, 2, 30);
-	wall23.color.Set(1.0f, 0.0f, 0.0f);
+	wall23.color.Set(0.6f, 0.2f, 1.0f);
 	wall23.SetPos(40, 10, 260);
 	wall23.Render();
 
 	Cube wall24(30, 2, 100);
-	wall24.color.Set(1.0f, 0.0f, 0.0f);
+	wall24.color.Set(0.6f, 0.2f, 1.0f);
 	wall24.SetPos(-35, 10, 295);
 	wall24.Render();
 
 	Cube wall25(10, 2, 100);
-	wall25.color.Set(1.0f, 0.0f, 0.0f);
+	wall25.color.Set(0.6f, 0.2f, 1.0f);
 	wall25.SetPos(-35, 10, 395);
 	wall25.Render();
 
 	Cube wall26(80, 2, 10);
-	wall26.color.Set(1.0f, 0.0f, 0.0f);
+	wall26.color.Set(0.6f, 0.2f, 1.0f);
 	wall26.SetPos(0, 10, 440);
 	wall26.Render();
 
 	Cube wall27(10, 2, 100);
-	wall27.color.Set(1.0f, 0.0f, 0.0f);
+	wall27.color.Set(0.6f, 0.2f, 1.0f);
 	wall27.SetPos(40, 10, 485);
 	wall27.Render();
 
 	Cube wall28(10, 2, 110);
-	wall28.color.Set(1.0f, 0.0f, 0.0f);
+	wall28.color.Set(0.6f, 0.2f, 1.0f);
 	wall28.SetPos(40, 10, 620);
 	wall28.Render();
 
